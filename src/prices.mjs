54770dd5d73fbf0247a41d6d635459a60f937e0c
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-03-13 16:48:49 +0200

    step 16

--- src/prices.mjs
+++ src/prices.mjs
@@ -83,7 +83,7 @@ function createApp(database) {
   function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = Temporal.PlainDate.from(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
