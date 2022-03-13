09f7b7db4bf07dd8a5fdffc3196f5e8e2018bf74
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-03-13 16:43:59 +0200

    step 13

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,7 +87,7 @@ function createApp(database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
+        date2.year === holiday2.year &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
